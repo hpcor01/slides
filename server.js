2eb960893e5bb4188df03d58256ab84a11b0c302
hpcor01 <119982304+hpcor01@users.noreply.github.com>
2025-08-31 18:06:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const PORT = process.env.PORT || 3000;
+
 mongoose.connect(process.env.MONGO_URL)
-  .then(()=>console.log("MongoDB conectado"))
-  .catch(err=>console.error(err));
+  .then(() => console.log("MongoDB conectado"))
+  .catch(err => console.error(err));
 
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
