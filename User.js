2a363c8a7426960f6fc110a5f59fc96adfd17cdb
hpcor01 <119982304+hpcor01@users.noreply.github.com>
2025-09-12 14:17:05 -0300

    Update User.js

--- User.js
+++ User.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  usermail: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  level: { type: Number, default: 0 } // 0 = comum, 1 = admin
+const userSchema = new mongoose.Schema({
+  username: String,
+  usermail: String,
+  password: String,
+  level: Number
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema, "colUser");
+// --> apontando para a collection colUser
